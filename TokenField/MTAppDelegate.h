124985df1c6ccf58b5f899e3f17b11633afdadb6
Scott Morrison <smorr@indev.ca>
2011-11-29 20:49:40 -0500

    Initial Commit

---
+++ TokenField/MTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MTAppDelegate.h
+//  TokenField
+//
+//  Created by smorr on 11-11-29.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MTAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ TokenField/MTAppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  MTAppDelegate.m
+//  TokenField
+//
+//  Created by smorr on 11-11-29.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "MTAppDelegate.h"
+
+@implementation MTAppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
