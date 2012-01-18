3dca9b7d7a07bb2a895d70c95aff6d23eb05d6ec
Scott Morrison <smorr@indev.ca>
2012-01-18 12:08:24 -0500

    Clean up code to rid of cruft

--- TokenField/MTAppDelegate.h
+++ TokenField/MTAppDelegate.h
@@ -7,9 +7,14 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "MTTokenField.h"
 
-@interface MTAppDelegate : NSObject <NSApplicationDelegate>
-
+@interface MTAppDelegate : NSObject <NSApplicationDelegate,NSTableViewDataSource,MTTokenFieldDelegate,NSTokenFieldDelegate>
+@property (retain) NSMutableArray* tokensForCompletion;
+@property (assign) IBOutlet MTTokenField *myTokenField;
+@property (assign) IBOutlet NSTokenField *standardTokenField;
+@property (assign) IBOutlet NSTableView *tableView;
+@property (assign) NSInteger option;
 @property (assign) IBOutlet NSWindow *window;
-
+-(IBAction)click:(id)sender;
 @end
--- TokenField/MTAppDelegate.m
+++ TokenField/MTAppDelegate.m
@@ -9,8 +9,11 @@
 #import "MTAppDelegate.h"
 
 @implementation MTAppDelegate
-
+@synthesize myTokenField;
+@synthesize standardTokenField,tableView;
 @synthesize window = _window;
+@synthesize tokensForCompletion;
+@synthesize option;
 
 - (void)dealloc
 {
@@ -19,7 +22,82 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+    self.tokensForCompletion = [NSMutableArray arrayWithObjects:@"A very long keyword",@"Änger",@"Blatt",@"test",@"tiara",@"typhoon",@"trick",@"trigger",@"🕚tiger",@"@tickle",@"@waiting",@"@Followup",@"Walrus", nil];  
+    
+    
+    [self.tableView reloadData];
+        
+    
+    
+    
     // Insert code here to initialize your application
+}
+-(NSInteger)numberOfRowsInTableView:(NSTableView *)tableView{
+    return [self.tokensForCompletion count];
+}
+
+-(id)tableView:(NSTableView *)tableView objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row   {
+    return [self.tokensForCompletion  objectAtIndex:row];
+}
+
+
+- (NSArray *)tokenField:(NSTokenField *)tokenField completionsForSubstring:(NSString *)substring indexOfToken:(NSInteger)tokenIndex indexOfSelectedItem:(NSInteger *)selectedIndex{
+    return [self tokenField:tokenField completionsForSubstring:substring];
+}
+- (NSArray *)tokenField:(MTTokenField *)tokenField completionsForSubstring:(NSString *)substring 
+{
+    NSArray * testArray = self.tokensForCompletion;
+    if (self.option==0){
+        NSMutableArray * keywordsMatchingString = [[[NSMutableArray alloc] init] autorelease];
+        for (NSString *aKeyword in testArray){
+            if ([aKeyword rangeOfString:substring options:NSCaseInsensitiveSearch].location !=NSNotFound){
+                [keywordsMatchingString addObject:aKeyword];  
+            }
+        }
+        return keywordsMatchingString;
+        
+    }
+    else{
+    
+        NSRange alphaNumericRange = [substring rangeOfCharacterFromSet:[NSCharacterSet alphanumericCharacterSet]];
+        NSString * alphaSubstring = @"";
+        
+        BOOL searchFullString = NO;
+        if (alphaNumericRange.location!=NSNotFound){
+            alphaSubstring = [substring substringFromIndex:alphaNumericRange.location];
+        }
+        else{
+             alphaSubstring = substring;
+            searchFullString = YES;
+        }
+        
+        NSMutableArray * keywordsMatchingString = [[[NSMutableArray alloc] init] autorelease];;
+        for (NSString *aKeyword in testArray){
+            if ([tokenField respondsToSelector:@selector(tokenArray)]){
+                if([[tokenField tokenArray] containsObject:aKeyword]) continue;
+            }
+            else{
+                if([[tokenField objectValue] containsObject:aKeyword]) continue;
+            }
+            
+            NSRange alphaNumericRange = [aKeyword rangeOfCharacterFromSet:[NSCharacterSet alphanumericCharacterSet]];
+            if (alphaNumericRange.location!=NSNotFound){
+                NSString * alphaKeyword = searchFullString?aKeyword:[aKeyword substringFromIndex:alphaNumericRange.location];
+                NSRange substringRange = [alphaKeyword rangeOfString:alphaSubstring options:NSCaseInsensitiveSearch|NSDiacriticInsensitiveSearch];
+                if (substringRange.location == 0){
+                    [keywordsMatchingString addObject:aKeyword];
+                }
+            }
+           
+        }
+     
+    return keywordsMatchingString; 
+    }
+        
+}
+
+-(IBAction)click:(id)sender{
+    [myTokenField setTokenArray:[NSArray arrayWithObject:@"blatt"]];
 }
 
 @end
